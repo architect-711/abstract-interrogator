56fc84862b5c7eac5bf9db47c2194c6674f88017
Architect <jfjjjnqhqiriykkn@gmail.com>
2024-02-26 09:58:13 +0300

    refoctor: change package

--- src/main/java/com/abstract_interrogator/AbstractInterrogator.java
+++ src/main/java/com/abstract_interrogator/AbstractInterrogator.java
@@ -1,5 +1,7 @@
+package com.abstract_interrogator;
+
 public class AbstractInterrogator {
     public static void start() {
-        
+
     }
 }
--- src/main/java/com/abstract_interrogator/Main.java
+++ src/main/java/com/abstract_interrogator/Main.java
@@ -1,3 +1,5 @@
+package com.abstract_interrogator;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Ponasenkov.");
