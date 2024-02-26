89b8ea2d92da5374284826377dd2b7605691d33d
Architect <jfjjjnqhqiriykkn@gmail.com>
2024-02-26 11:36:43 +0300

    fix(InterrogatorTets): change exception class
    assertThrows's exception class is IllegalArgumentException

--- src/test/java/com/abstract_interrogator/InterrogatorTest.java
+++ src/test/java/com/abstract_interrogator/InterrogatorTest.java
@@ -11,6 +11,6 @@ class InterrogatorTest {
     void shouldThrowIllegalArgumentException() {
         Interrogator interrogator = new Interrogator(new HashSet<>());
 
-        assertThrows(IllegalAccessError.class, interrogator::startInterrogation);
+        assertThrows(IllegalArgumentException.class, interrogator::startInterrogation);
     }
 }
