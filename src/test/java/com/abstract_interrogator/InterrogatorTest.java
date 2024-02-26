ef7b4e259421555a3ca012d5b1eea382eaf228c9
Architect <jfjjjnqhqiriykkn@gmail.com>
2024-02-26 11:30:23 +0300

    test(InterrogatorTest): add test of empty set
    Pass empty HasSet and expect IllegalArgumentException

--- src/test/java/com/abstract_interrogator/InterrogatorTest.java
+++ src/test/java/com/abstract_interrogator/InterrogatorTest.java
@@ -1,7 +1,16 @@
 package com.abstract_interrogator;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class InterrogatorTest {
+    @Test
+    void shouldThrowIllegalArgumentException() {
+        Interrogator interrogator = new Interrogator(new HashSet<>());
 
+        assertThrows(IllegalAccessError.class, () -> interrogator.startInterrogation());
+    }
 }
