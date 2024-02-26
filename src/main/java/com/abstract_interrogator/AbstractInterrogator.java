691bb2d460966a29e2ac603590a74ffea0bece71
Architect <jfjjjnqhqiriykkn@gmail.com>
2024-02-26 11:02:37 +0300

    feat: add calling DataParser in AI start method

--- src/main/java/com/abstract_interrogator/AbstractInterrogator.java
+++ src/main/java/com/abstract_interrogator/AbstractInterrogator.java
@@ -2,6 +2,14 @@ package com.abstract_interrogator;
 
 public class AbstractInterrogator {
     public static void start() {
+        try {
+            DataParser dataParser = new DataParser();
 
+            dataParser.findFile();
+            dataParser.parseData();
+            System.out.println(dataParser.getParsedData());
+        } catch (Exception exception) {
+            System.out.println(exception);
+        }
     }
 }
