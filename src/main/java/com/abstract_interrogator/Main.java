bbe4afb1bdbc543d5b0abdd4041210f501b79b42
Architect <jfjjjnqhqiriykkn@gmail.com>
2024-02-28 08:01:49 +0300

    feat: add passing args from main to start method

--- src/main/java/com/abstract_interrogator/Main.java
+++ src/main/java/com/abstract_interrogator/Main.java
@@ -2,6 +2,6 @@ package com.abstract_interrogator;
 
 public class Main {
     public static void main(String[] args) {
-        AbstractInterrogator.start();
+        AbstractInterrogator.start(args);
     }
 }
